742418795e2729eddf0c618b73ac373e827cdaa7
senth-srmist <senthilr4@srmist.edu.in>
2022-08-30 03:57:15 -0400

    three files newly added

--- 01_rearrange1.py
+++ 01_rearrange1.py
@@ -1,8 +1,9 @@
 import re
 def rearrange_name(name):
-	result = re.search(r"^([\w .]*), ([\w .]*)$",name)
+	result = re.search(r"^([\w .-]*), ([\w .-]*)$",name)
+	print('the change')	
 	if result==None:
 		return result
-
 	return "{} {}".format(result[2], result[1])
-print('checking')
+	print('a')
+
